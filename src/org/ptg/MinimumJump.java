ec06f222b8d43bb50b63783b37b2f877f7a1ad3a
Paiman Irani <paimanirani@yahoo.com>
2019-06-06 19:44:08 -0700

    initial implementaion

---
+++ src/org/ptg/MinimumJump.java
@@ -0,0 +1,101 @@
+package org.ptg;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * If adding new index along with the value of that index is greater than the
+ * length of the array, then we store that index and the end index of the array
+ * by doing that, we are performing 2 jumps.
+ * Otherwise, we pick the index of the maximum value to achieve minimum jumps
+ * 
+ * example1: { 1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9 };
+ * values=[1, 3]
+ * newIndex = 3
+ * arr[newIndex] = 8
+ * newIndex + arr[newIndex]>=10
+ * (we add - arr[newIndex] and arr[arr.length - 1])
+ * 
+ * Therefore, values=[1,3,8,9]
+ * ---------------------------------------------------------------------------------
+ * example2: { 2, 3, 1, 1, 2, 4, 2, 0, 1, 1 };
+ * values=[2]
+ * (We have 2 scenarios-> newIndex =  currentIndex + i
+ * newIndex = 0+1 = 1
+ * newIndex = 0+2 = 2
+ * 
+ * Since, arr[1]>arr[2])
+ * maxIndex=1
+ * currentIndex=maxIndex
+ * values=[2,3]
+ * 
+ * 
+ */
+
+public class MinimumJump {
+	static void minJumps(int arr[]) {
+		List<Integer> values = new ArrayList<>();
+		int currentIndex = 0;
+		int jump = 0;
+
+		if (arr.length <= 1) {
+			System.out.println("jump=" + 0);
+			return;
+		}
+		// Return -1 if not possible to jump
+		if (arr[0] == 0) {
+			System.out.println("Impossible to jump");
+			return;
+		}
+		while (currentIndex < arr.length - 1) {
+			int maxIndex = -1;
+			int newIndex = -1;
+			values.add(arr[currentIndex]);
+			for (int i = 1; i <= arr[currentIndex]; i++) {
+				newIndex = currentIndex + i;
+				if (maxIndex == -1) {
+					maxIndex = newIndex;
+				}
+				if (newIndex + arr[newIndex] >= arr.length - 1) {
+					values.add(arr[newIndex]);
+					currentIndex = arr.length - 1;
+					values.add(arr[currentIndex]);
+					jump += 2;
+					break;
+				} else {
+					if (arr[maxIndex] <= arr[newIndex]) {
+						maxIndex = newIndex;
+					}
+				}
+			}
+			if (currentIndex != arr.length - 1) {
+				currentIndex = maxIndex;
+				jump++;
+			}
+
+		}
+
+		System.out.println(values);
+		System.out.println(jump);
+		System.out.println("\n");
+
+	}
+
+	// Driver method to test the above function
+	public static void main(String[] args) {
+		int arr[] = new int[] { 1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9 };
+
+		// calling minJumps method
+		minJumps(arr);
+
+		// example2
+		arr = new int[] { 2, 3, 1, 1, 2, 4, 2, 0, 1, 1 };
+		minJumps(arr);
+
+		// example3
+		arr = new int[] { 2, 1, 3, 2, 3, 4, 5, 1, 2, 8 };
+		minJumps(arr);
+
+	}
+
+}
